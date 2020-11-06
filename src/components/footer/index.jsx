de46fde5dca1d5053fa9e5c673b38d77adb6abca
23carnies <46233990+23carnies@users.noreply.github.com>
2020-11-06 15:42:29 -0500

    Add footer

---
+++ src/components/footer/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import styles from './footer.module.css'
+
+export default function Footer() {
+    return (
+        <footer className={styles.footer}>
+            <p className={styles.p} id={styles.small}>&copy; {new Date().getFullYear()} Not in Kansas, Quilts by Dorothy</p>
+            <p className={styles.p}>Site design by 23carnies</p>
+        </footer>
+    )
+}
--- src/components/layout/index.jsx
+++ src/components/layout/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import styles from './layout.module.css'
 import NavBar from '../navbar'
+import Footer from '../footer'
 
 
 export default function Layout({ children }) {
@@ -9,6 +10,7 @@ export default function Layout({ children }) {
             <main>
             <NavBar />
                 {children}
+            <Footer />
             </main>
         </div>
     )
