5ab976eb81884e300a6bd0cfbd581ef4c08a64ea
23carnies <46233990+23carnies@users.noreply.github.com>
2020-11-06 15:41:03 -0500

    Add navbar

--- src/components/layout/index.jsx
+++ src/components/layout/index.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import styles from './layout.module.css'
+import NavBar from '../navbar'
 
 
 export default function Layout({ children }) {
     return (
         <div className={styles.layout}>
             <main>
+            <NavBar />
                 {children}
             </main>
         </div>
---
+++ src/components/navbar/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Link } from 'gatsby'
+import styles from './navbar.module.css'
+
+export default function NavBar() {
+    return (
+        <nav className={styles.nav}>
+            <Link to="/" id={styles.siteTitle}>Not in Kansas, Quilts by Dorothy</Link>
+            <Link to="/memory">Memory Quilts</Link>
+            <Link to="/design">Designed Quilts</Link>
+            <Link to="/testimonials">Testimonials</Link>
+            <Link to="/contact">Contact</Link>
+        </nav>
+    )
+} 
