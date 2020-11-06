fc2bf4b0c5c44a92457bd8f908506a0ea6abc013
23carnies <46233990+23carnies@users.noreply.github.com>
2020-11-06 15:37:47 -0500

    Add layout

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
+import Layout from '../components/layout'
 import Header from '../components/header'
 
 export default function Contact() {
     return (
         <>
+            <Layout>
             
             <Header headerText="Questions? Get in touch!"/>
+            </Layout>
         </>
     )
 }
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,15 +1,18 @@
 import React from "react"
 import Header from "../components/header"
+import Layout from "../components/layout"
 
 export default function Home() {
   return (
     <div>
+      <Layout>
       <Header headerText="Quality memory quilts and designed quilts"/>
       <h2>Made with care by Dorothy in Atlanta, Ga.</h2>
       <p>All of my quilts are made with top quality, 100% cotton materials, machine pieced by me with all the care and skill I can bring
                 to each project. The most important thing to me is for you to be happy with your finished product. All quilts, except for small projects,
                 are Long Arm Quilted by my close friend Glenna Ann Bowmaster of Fine Feathers Quilting. She works her magic, then I do the finish work.
                 (All binding sewn down by hand.)</p>
+    </Layout>
     </div>
   )
 }
