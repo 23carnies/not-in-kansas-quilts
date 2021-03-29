58aa5fa321d569b1d0c210a3006e3a2d868fa5d9
23carnies <46233990+23carnies@users.noreply.github.com>
2021-03-29 17:39:21 -0400

    Add email functionality

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -1,101 +1,69 @@
 import React, { Component, form } from 'react';
 import Header from '../components/header'
-import * as emailjs from 'emailjs-com'
 import styles from './index.module.css'
 import NavBar from '../components/navbar';
 import Footer from '../components/footer';
+import emailjs from 'emailjs-com';
 
-class Contact extends Component {
-    state = { 
-        name: '', 
-        email: '',
-        subject: '',
-        message: '',
-     }
+export default function ContactUs() {
 
-    handleSubmit(e) {
-        e.preventDefault()
+  function sendEmail(e) {
+    e.preventDefault();
 
-        const { name, email, subject, message } = this.state
+    emailjs.sendForm('service_rq61ih6', 'template_ypi3a1g', e.target, 'user_RpE4R9r6kaSRMl4E0Jssd')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+  }
 
-        let templateParams = {
-            from_name: email,
-            to_name: '<YOUR_EMAIL_ID',
-            subject: subject,
-            message_html: message,
-        }
+  return (
+    <>
+    <NavBar />
+        <main className={styles.main}>
+        <Header headerText="Questions? Get in touch!"/>
+        <p>I'm excited to talk to you about a memory quilt gift, the new design quilt to match your bedroom, or a quilt gift for a baby or child. </p>
+        <form className={styles.contactform} onSubmit={sendEmail}>
+            <input type="hidden" name="contact_number" />
+            <label className={styles.label}>Email</label>
+            <input type="email" 
+            className={styles.input}
+            name="email"
+            // value={this.state.email} 
+            // onChange={this.handleChange}
+            placeholder="jane@me.com"
+            />
+            <label className={styles.label}>Name</label>
+            <input type="text"
+            className={styles.input}
+            name="name"
+            // value={this.state.name}
+            // onChange={this.handleChange}
+            placeholder="Jane Doe"
+            />
+            <label className={styles.label}>Subject</label>
+            <input type="text"
+            className={styles.input}
+            name="subject"
+            // value={this.state.subject}
+            // onChange={this.handleChange}
+            placeholder="Subject"
+            />
+            <label className={styles.label}>Message</label>
+            <textarea type="text"
+            className={styles.input}
+            name="message"
+            // value={this.state.message}
+            // onChange={this.handleChange}
+            placeholder="Message"
+            />
+            <input className={styles.button} type="submit" value="Send" />
+        </form>
+        </main>
+    <Footer />
+    </>
 
-        emailjs.send(
-            'gmail',
-            'template_XXXXXXXX',
-            templateParams,
-            'user_XXXXXXXXXXXXXXXXXXX'
-        )
-
-        this.resetForm()
-    }
-
-    resetForm() {
-        this.setState({
-            name: '',
-            email: '',
-            subject: '',
-            message: '',
-        })
-    }
-
-    handleChange = (param, e) => {
-        this.setState({ [param]: e.target.value })
-    }
-    render() { 
-        return ( 
-        <>
-        <NavBar />
-            <main className={styles.main}>
-            <Header headerText="Questions? Get in touch!"/>
-            <p>I'm excited to talk to you about a memory quilt gift, the new design quilt to match your bedroom, or a quilt gift for a baby or child. </p>
-            <form className={styles.contactform} onSubmit={this.handleSubmit}>
-                <input type="hidden" name="contact_number" />
-                <label className={styles.label}>Email</label>
-                <input type="email" 
-                className={styles.input}
-                name="email"
-                value={this.state.email} 
-                onChange={this.handleChange}
-                placeholder="jane@me.com"
-                />
-                <label className={styles.label}>Name</label>
-                <input type="text"
-                className={styles.input}
-                name="name"
-                value={this.state.name}
-                onChange={this.handleChange}
-                placeholder="Jane Doe"
-                />
-                <label className={styles.label}>Subject</label>
-                <input type="text"
-                className={styles.input}
-                name="subject"
-                value={this.state.subject}
-                onChange={this.handleChange}
-                placeholder="Subject"
-                />
-                <label className={styles.label}>Message</label>
-                <textarea type="text"
-                className={styles.input}
-                name="message"
-                value={this.state.message}
-                onChange={this.handleChange}
-                placeholder="Message"
-                />
-                <input className={styles.button} type="submit" value="Send" />
-            </form>
-            </main>
-        <Footer />
-        </>
-         );
-    }
+  );
 }
- 
-export default Contact;
 
